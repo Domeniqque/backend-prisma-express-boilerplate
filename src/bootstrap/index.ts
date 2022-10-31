d9a9efeaa8cbf0a5d68d9204f933bf25810302be
Domeniqque Dylluar <domeniqque@hotmail.com>
2022-10-30 20:49:44 -0400

    enable cors

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -2,12 +2,14 @@ import "reflect-metadata";
 import "express-async-errors";
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import { loggerMiddleware } from "~/logger";
 import { globalErrorHandling } from "~/app/http/middleware/globalErrorHandling.middleware";
 import { startRoutes } from "./router";
 
 const App = express();
 
+App.use(cors());
 App.use(express.json({ limit: "100kb" })); // default 100kb
 App.use(express.urlencoded({ extended: true, limit: "100kb" })); // default 100kb
 App.use(globalErrorHandling);
